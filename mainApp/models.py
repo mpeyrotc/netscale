1535f0382cde86923d60f3dd8472740ac1514e71
Marco Peyrot <mpeyrotc@dhcp-10-16-177-183.mty.itesm.mx>
2017-02-27 00:00:54 -0600

    add user model

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    # reference to django's user model
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    password = models.CharField(max_length=50)
+    gmail_id = models.CharField(max_length=30)
