2587b3b4ae04bbb2b8cb00e069fcbbb2e43a2e75
Marco Peyrot <mpeyrotc@Marcos-MacBook-Pro-2.local>
2017-07-17 20:06:50 -0500

    common domain off

--- mainApp/views.py
+++ mainApp/views.py
@@ -27,6 +27,29 @@ def get_user(request):
 def get_profile(request):
     return UserProfile.objects.filter(user__exact=get_user(request))[0]
 
+common_domains = [
+  "aol.com", "att.net", "comcast.net", "facebook.com", "gmail.com", "gmx.com", "googlemail.com",
+  "gmail", "hotmail", "hotmail.co.uk", "mac", "me", "mail", "msn", "yahoo",
+  "live.com", "sbcglobal.net", "verizon.net", "yahoo.com", "yahoo.co.uk",
+  "email.com", "games.com", "gmx.net", "hush.com", "hushmail.com", "icloud.com", "inbox.com",
+  "lavabit.com", "love.com", "outlook.com", "pobox.com", "rocketmail.com",
+  "safe-mail.net", "wow.com", "ygm.com", "ymail.com", "zoho.com", "fastmail.fm",
+  "yandex.com","iname.com",
+  "bellsouth.net", "charter.net", "cox.net", "earthlink.net", "juno.com",
+  "btinternet.com", "virginmedia.com", "blueyonder.co.uk", "freeserve.co.uk", "live.co.uk",
+  "ntlworld.com", "o2.co.uk", "orange.net", "sky.com", "talktalk.co.uk", "tiscali.co.uk",
+  "virgin.net", "wanadoo.co.uk", "bt.com",
+  "sina.com", "qq.com", "naver.com", "hanmail.net", "daum.net", "nate.com", "yahoo.co.jp", "yahoo.co.kr",
+              "yahoo.co.id", "yahoo.co.in", "yahoo.com.sg", "yahoo.com.ph",
+  "hotmail.fr", "live.fr", "laposte.net", "yahoo.fr", "wanadoo.fr", "orange.fr", "gmx.fr", "sfr.fr", "neuf.fr", "free.fr",
+  "gmx.de", "hotmail.de", "live.de", "online.de", "t-online.de", "web.de", "yahoo.de",
+  "mail.ru", "rambler.ru", "yandex.ru", "ya.ru", "list.ru",
+  "hotmail.be", "live.be", "skynet.be", "voo.be", "tvcablenet.be", "telenet.be",
+  "hotmail.com.ar", "live.com.ar", "yahoo.com.ar", "fibertel.com.ar", "speedy.com.ar", "arnet.com.ar",
+  "yahoo.com.mx", "live.com.mx", "hotmail.es", "hotmail.com.mx", "prodigy.net.mx",
+  "yahoo.com.br", "hotmail.com.br", "outlook.com.br", "uol.com.br", "bol.com.br", "terra.com.br", "ig.com.br",
+    "itelefonica.com.br", "r7.com", "zipmail.com.br", "globo.com", "globomail.com", "oi.com.br"
+]
 
 @login_required
 def home(request):
@@ -42,7 +65,7 @@ def home(request):
 
         search_token = search_token.strip().lower()
 
-        if profile.friends:
+        if profile.friends and search_token not in common_domains:
             friends = Friend.objects.friends(request.user)
 
             for friend in friends:
